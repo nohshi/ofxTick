36717589704003319083a070330b18be5f5bb381
nohshi <awukihc9@gmail.com>
2020-12-29 14:21:35 +0900

    add addon_comfig

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxTick
+	ADDON_DESCRIPTION = An addon to implement a function that runs at regular intervals
+	ADDON_AUTHOR = kyosuke fujita
+	ADDON_TAGS = "Utilities"
+	ADDON_URL = https://github.com/nohshi/ofxTick
